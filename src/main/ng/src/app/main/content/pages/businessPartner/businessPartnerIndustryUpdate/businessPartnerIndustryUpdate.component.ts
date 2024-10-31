8ef5e483152a22ebd83509fef7e59ceb5a369229
Gopinath Guptha <gopinath.br@gmail.com>
2024-10-31 18:41:16 +0530

    feat: add business partner/ identification functionality

--- src/main/ng/src/app/main/content/pages/businessPartner/businessPartnerIndustryUpdate/businessPartnerIndustryUpdate.component.ts
+++ src/main/ng/src/app/main/content/pages/businessPartner/businessPartnerIndustryUpdate/businessPartnerIndustryUpdate.component.ts
@@ -15,7 +15,7 @@ export class BusinessPartnerIndustryUpdateComponent implements OnInit {
 
     dialogTitle = 'Add Industry';
 
-    selectedIndustry: any;
+    selectedBusinessPartnerIndustry: any;
 
     industryUpdateForm: FormGroup;
     industrySystems: any;
@@ -32,12 +32,15 @@ export class BusinessPartnerIndustryUpdateComponent implements OnInit {
 
         // Fetch list of industry systems and other details from the dialog's data attribute.
         this.industrySystems = this._dialogData.industrySystems;
-        if (_dialogData.selectedIndustry !== undefined) {
-            this.selectedIndustry = Object.assign({}, _dialogData.selectedIndustry);
+        if (_dialogData.selectedBusinessPartnerIndustry !== undefined) {
+            this.selectedBusinessPartnerIndustry = Object.assign({}, _dialogData.selectedBusinessPartnerIndustry);
+            this._businessPartnerService.getIndustryTypes(this.selectedBusinessPartnerIndustry.industrySystemId).subscribe((response: any) => {
+                this.industryTypes = response._embedded.industryTypes;
+            });
             this.dialogTitle = 'Modify Industry';
         }
         else {
-            this.selectedIndustry = {};
+            this.selectedBusinessPartnerIndustry = {};
         }
     }
 
@@ -46,33 +49,38 @@ export class BusinessPartnerIndustryUpdateComponent implements OnInit {
      */
     ngOnInit(): void {
         this.industryUpdateForm = this._formBuilder.group({
-            serialNumber: [this.selectedIndustry.serialNumber || null],
-            industrySystemId: [this.selectedIndustry.industrySystemId || null],
-            industryTypeId: [this.selectedIndustry.industryTypeId || null],
+            serialNumber: [this.selectedBusinessPartnerIndustry.serialNumber || null],
+            industrySystemId: [this.selectedBusinessPartnerIndustry.industrySystemId || null],
+            industryTypeId: [this.selectedBusinessPartnerIndustry.industryTypeId || null],
         });
     }
     
+    /**
+     * fetchIndustryTypes()
+     */
     fetchIndustryTypes(event: any): void {
-        console.log('in fetch industry types');
-        console.log('event is', event);
+        this._businessPartnerService.getIndustryTypes(event.value).subscribe((response: any) => {
+            this.industryTypes = response._embedded.industryTypes;
+        });
     }
 
     /**
      * submit()
      */
     submit(): void {
+        console.log(this.selectedBusinessPartnerIndustry);
         if (this.industryUpdateForm.valid) {
-            if (this._dialogData.operation === 'addIndustry') {
-                this._businessPartnerService.createBusinessPartnerIndustry(this.selectedIndustry, this._dialogData.businessPartnerId).
+            if (this._dialogData.operation === 'addBusinessPartnerIndustry') {
+                this._businessPartnerService.createBusinessPartnerIndustry(this.industryUpdateForm.value, this._dialogData.businessPartnerId).
                         subscribe(() => {
                     this._matSnackBar.open('Industry details added successfully.', 'OK', { duration: 7000 });
                     this._dialogRef.close({ 'refresh': true });
                 });
             }
             else {
-                this.selectedIndustry.industrySystemId = this.industryUpdateForm.value.industrySystemId;
-                this.selectedIndustry.industryTypeId = this.industryUpdateForm.value.industryTypeId;
-                this._businessPartnerService.updateBusinessPartnerIndustry(this.selectedIndustry).subscribe(() => {
+                this.selectedBusinessPartnerIndustry.industrySystemId = this.industryUpdateForm.value.industrySystemId;
+                this.selectedBusinessPartnerIndustry.industryTypeId = this.industryUpdateForm.value.industryTypeId;
+                this._businessPartnerService.updateBusinessPartnerIndustry(this.selectedBusinessPartnerIndustry).subscribe(() => {
                     this._matSnackBar.open('Industry details updated successfully.', 'OK', { duration: 7000 });
                     this._dialogRef.close({ 'refresh': true });
                 });            
