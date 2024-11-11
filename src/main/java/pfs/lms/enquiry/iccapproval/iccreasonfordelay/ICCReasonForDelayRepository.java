719a063321f9f8ebeb33302f893d94cbc5129c74
Gopinath Guptha <gopinath.br@gmail.com>
2024-11-11 18:22:14 +0530

    fix: bug fixes ...

--- src/main/java/pfs/lms/enquiry/iccapproval/iccreasonfordelay/ICCReasonForDelayRepository.java
+++ src/main/java/pfs/lms/enquiry/iccapproval/iccreasonfordelay/ICCReasonForDelayRepository.java
@@ -2,9 +2,10 @@ package pfs.lms.enquiry.iccapproval.iccreasonfordelay;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ICCReasonForDelayRepository extends JpaRepository<ICCReasonForDelay, UUID> {
 
-    ICCReasonForDelay findByIccApprovalId(UUID iccApprovalId);
+    List<ICCReasonForDelay> findByIccApprovalId(UUID iccApprovalId);
 }
