cc593460161a14bec5e5fcdac803120ea7569031
Sajeev.khan@gmail.com <Koopil@5656@>
2024-11-12 10:48:01 +0530

    CreditRatingAgencyAndCode v1

--- src/main/java/pfs/lms/enquiry/businesspartner/config/CreditRatingAgencyConfig.java
+++ src/main/java/pfs/lms/enquiry/businesspartner/config/CreditRatingAgencyConfig.java
@@ -19,12 +19,13 @@ public class CreditRatingAgencyConfig implements CommandLineRunner {
 
         CreditRatingAgency creditRatingAgency = new CreditRatingAgency();
 
+
         creditRatingAgency = creditRatingAgencyRepository.findByCode("CRISIL");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CRISIL","CRISIL"); creditRatingAgencyRepository.save(creditRatingAgency); }
-        creditRatingAgency = creditRatingAgencyRepository.findByCode("ICRA");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CRISIL","ICRA"); creditRatingAgencyRepository.save(creditRatingAgency); }
-        creditRatingAgency = creditRatingAgencyRepository.findByCode("BWR");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CRISIL","BWR"); creditRatingAgencyRepository.save(creditRatingAgency); }
-        creditRatingAgency = creditRatingAgencyRepository.findByCode("IND");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CRISIL","IND"); creditRatingAgencyRepository.save(creditRatingAgency); }
-        creditRatingAgency = creditRatingAgencyRepository.findByCode("CARE");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CRISIL","CARE"); creditRatingAgencyRepository.save(creditRatingAgency); }
-        creditRatingAgency = creditRatingAgencyRepository.findByCode("Informics");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CRISIL","Informics"); creditRatingAgencyRepository.save(creditRatingAgency); }
+        creditRatingAgency = creditRatingAgencyRepository.findByCode("ICRA");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"ICRA","ICRA"); creditRatingAgencyRepository.save(creditRatingAgency); }
+        creditRatingAgency = creditRatingAgencyRepository.findByCode("BWR");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"BWR","BWR"); creditRatingAgencyRepository.save(creditRatingAgency); }
+        creditRatingAgency = creditRatingAgencyRepository.findByCode("IND");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"IND","IND"); creditRatingAgencyRepository.save(creditRatingAgency); }
+        creditRatingAgency = creditRatingAgencyRepository.findByCode("CARE");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"CARE","CARE"); creditRatingAgencyRepository.save(creditRatingAgency); }
+        creditRatingAgency = creditRatingAgencyRepository.findByCode("Informics");if (creditRatingAgency == null){ creditRatingAgency = new CreditRatingAgency(null,"Informics","Informics"); creditRatingAgencyRepository.save(creditRatingAgency); }
 
         return;
     }
