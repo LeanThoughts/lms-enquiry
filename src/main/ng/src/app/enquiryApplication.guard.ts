449c0e70c642444307e9b285cb4e56765ccf27da
Gopinath Guptha <gopinath.br@gmail.com>
2025-01-14 16:56:22 +1000

    change route guard to allow access to all user roles ...

--- src/main/ng/src/app/enquiryApplication.guard.ts
+++ src/main/ng/src/app/enquiryApplication.guard.ts
@@ -23,15 +23,17 @@ export class EnquiryApplicationRouteGuard implements CanActivate {
                     observer.next(false);
                 }
                 else {
-                    if (this.currentUser.role === 'TR0100' || this.currentUser.role === 'ZLM023' || this.currentUser.role === 'ZLM013' 
-                            || this.currentUser.role === 'ZLM024' || this.currentUser.role === 'ZLM040' || this.currentUser.role === 'ZLM014') {
-                        console.log('enquiry route guard - true');
-                        observer.next(true);
-                    }
-                    else {
-                        this._router.navigate(['enquiryAlerts']);
-                        observer.next(false);
-                    }
+                    console.log('enquiry route guard - true');
+                    observer.next(true);
+                    // if (this.currentUser.role === 'TR0100' || this.currentUser.role === 'ZLM023' || this.currentUser.role === 'ZLM013' 
+                    //         || this.currentUser.role === 'ZLM024' || this.currentUser.role === 'ZLM040' || this.currentUser.role === 'ZLM014') {
+                    //     console.log('enquiry route guard - true');
+                    //     observer.next(true);
+                    // }
+                    // else {
+                    //     this._router.navigate(['enquiryAlerts']);
+                    //     observer.next(false);
+                    // }
                 }
             });
         });
