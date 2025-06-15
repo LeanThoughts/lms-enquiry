8b12b2244fa63af65d25778889412f63e57ed83d
Sajeev.khan@gmail.com <Koopil@5656@>
2025-06-15 10:31:54 +0530

    SME FI Loan Product v2

--- src/main/java/pfs/lms/enquiry/configInitializer/ProductConfigInitializer.java
+++ src/main/java/pfs/lms/enquiry/configInitializer/ProductConfigInitializer.java
@@ -25,17 +25,17 @@ public class ProductConfigInitializer implements CommandLineRunner {
 
         //samendmentReason = amendmentReasonRepository.findByCode("1");if (amendmentReason == null){ amendmentReason = new AmendmentReason( "1","Change in Sanction Amt-Others"); amendmentReasonRepository.save(amendmentReason); }
 
-            product = productRepository.findByCode("301"); if (product == null) { Product pr1 = new Product("301", "Bridge Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("302"); if (product == null) { Product pr1 = new Product("302", "Short Term Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("303"); if (product == null) { Product pr1 = new Product("303", "Term Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("304"); if (product == null) { Product pr1 = new Product("304", "Debentures"); productRepository.save(product); };
-            product = productRepository.findByCode("305"); if (product == null) { Product pr1 = new Product("305", "Non Fund Based Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("30F"); if (product == null) { Product pr1 = new Product("30F", "Facilities"); productRepository.save(product); };
-            product = productRepository.findByCode("310"); if (product == null) { Product pr1 = new Product("310", "Facilities Drawdown-NFB Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("311"); if (product == null) { Product pr1 = new Product("311", "Facilities Drawdown-Term Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("320"); if (product == null) { Product pr1 = new Product("320", "SME Term Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("321"); if (product == null) { Product pr1 = new Product("321", "FI Term Loan"); productRepository.save(product); };
-            product = productRepository.findByCode("991"); if (product == null) { Product pr1 = new Product("991", "Short Term Loan for Vehicle"); productRepository.save(product); };
+            product = productRepository.findByCode("301"); if (product == null) { Product pr1 = new Product("301", "Bridge Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("302"); if (product == null) { Product pr1 = new Product("302", "Short Term Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("303"); if (product == null) { Product pr1 = new Product("303", "Term Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("304"); if (product == null) { Product pr1 = new Product("304", "Debentures"); productRepository.save(pr1); };
+            product = productRepository.findByCode("305"); if (product == null) { Product pr1 = new Product("305", "Non Fund Based Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("30F"); if (product == null) { Product pr1 = new Product("30F", "Facilities"); productRepository.save(pr1); };
+            product = productRepository.findByCode("310"); if (product == null) { Product pr1 = new Product("310", "Facilities Drawdown-NFB Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("311"); if (product == null) { Product pr1 = new Product("311", "Facilities Drawdown-Term Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("320"); if (product == null) { Product pr1 = new Product("320", "SME Term Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("321"); if (product == null) { Product pr1 = new Product("321", "FI Term Loan"); productRepository.save(pr1); };
+            product = productRepository.findByCode("991"); if (product == null) { Product pr1 = new Product("991", "Short Term Loan for Vehicle"); productRepository.save(pr1); };
 
         return;
     }
