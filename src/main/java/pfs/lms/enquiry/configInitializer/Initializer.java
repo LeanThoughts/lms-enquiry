057f20cd2ae5db2fadc77f77f2d18d348c9a2456
Sajeev.khan@gmail.com <Koopil@5656@>
2025-07-31 09:43:47 +0530

    Partner Address Max Length 40

--- src/main/java/pfs/lms/enquiry/configInitializer/Initializer.java
+++ src/main/java/pfs/lms/enquiry/configInitializer/Initializer.java
@@ -263,8 +263,8 @@ public class Initializer implements CommandLineRunner {
 
         purposeOfLoanRepository.deleteAll();
 
-        PurposeOfLoan p1 = new PurposeOfLoan("1","Term Loan");
-        PurposeOfLoan p2 = new PurposeOfLoan("10","Term Loan/Sub Debt");
+//        PurposeOfLoan p1 = new PurposeOfLoan("1","Term Loan");
+//        PurposeOfLoan p2 = new PurposeOfLoan("10","Term Loan/Sub Debt");
         PurposeOfLoan p3 = new PurposeOfLoan("1a","TL-Term Loan");
         PurposeOfLoan p4 = new PurposeOfLoan("1b","TL - Term Loan & Non-fund bas");
         PurposeOfLoan p5 = new PurposeOfLoan("1c","TL Cost Overrun Funding");
@@ -306,7 +306,7 @@ public class Initializer implements CommandLineRunner {
         PurposeOfLoan p41 = new PurposeOfLoan("8","Term Loan/Take out financing");
         PurposeOfLoan p42 = new PurposeOfLoan("9","Term Loan/Top Up Loan");
 
-        purposeOfLoanRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33,p34,p35,p36,p37,p38,p39,p40,p41,p42));
+        purposeOfLoanRepository.saveAll(Arrays.asList(p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33,p34,p35,p36,p37,p38,p39,p40,p41,p42));
         log.info("Loan Purposes  ");
 
 
