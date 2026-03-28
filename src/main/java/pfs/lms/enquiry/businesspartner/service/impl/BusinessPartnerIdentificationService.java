306d7afb2dcce30466702a4c8e0092129097907e
Sajeev.khan@gmail.com <Koopil@5656@>
2026-03-28 09:51:31 +0530

    Bupa Integration v10

--- src/main/java/pfs/lms/enquiry/businesspartner/service/impl/BusinessPartnerIdentificationService.java
+++ src/main/java/pfs/lms/enquiry/businesspartner/service/impl/BusinessPartnerIdentificationService.java
@@ -383,7 +383,7 @@ public class BusinessPartnerIdentificationService implements IBusinessPartnerIde
                         findByCountryCode(businessPartnerIdentification.getCountry()).getValue());
 
             if (businessPartnerIdentification.getRegion() != null)
-                businessPartnerIdentificationResource.setRegionName(regionRepository.findByCountryCodeAnAndRegionCode(businessPartnerIdentification.getCountry(),
+                businessPartnerIdentificationResource.setRegionName(regionRepository.findByCountryCodeAndRegionCode(businessPartnerIdentification.getCountry(),
                                 businessPartnerIdentification.getRegion()).getValue());
 
             if (businessPartnerIdentification.getDocumentType() != null && businessPartnerIdentification.getDocumentType().length() > 0 )
